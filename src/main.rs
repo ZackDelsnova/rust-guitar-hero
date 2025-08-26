4e05d6283117b697b972a8a9345348d277f1b213
ZackDelsnova <96685170+ZackDelsnova@users.noreply.github.com>
2025-08-27 01:04:38 +0530

    something working

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,36 @@ use ggez::{ Context, GameResult };
 use ggez::glam::*;
 
 struct GameState {
-    note_y: f32,
-
     // TODO: Add your systems here
     // audio_manager: AudioManager,
     // note_spawner: NoteSpawner,
     // input_manager: InputManager,
     // score_manager: ScoreManager,
+    note_y: f32,
+    hit_line_y: f32,
+    score: i32,
 }
 
 impl GameState {
-    fn new() -> GameResult<GameState> {
+    fn new(ctx: &mut Context) -> GameResult<GameState> {
+        let (_w, h) = ctx.gfx.drawable_size(); 
         
-        Ok(GameState { note_y: 0.0 })
+        Ok(GameState {
+            note_y: 0.0,
+            hit_line_y: h * 0.8,
+            score: 0,
+        })
     }
 }
 
 impl event::EventHandler<ggez::GameError> for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        let _dt = ctx.time.delta().as_secs_f32();
-
+        let dt = ctx.time.delta().as_secs_f32();
+        self.note_y += 200.0 * dt;
+        if self.note_y > self.hit_line_y + 100.0 {
+            self.note_y = 0.0;
+            self.score -= 10;
+        }
         // Update systems here
         // self.audio_manager.update(dt);
         // let song_time = self.audio_manager.current_time();
@@ -35,24 +45,58 @@ impl event::EventHandler<ggez::GameError> for GameState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let (win_w, _win_h) = ctx.gfx.drawable_size();
         let mut canvas = graphics::Canvas::from_frame(
             ctx,
             graphics::Color::from([0.1, 0.2, 0.3, 1.0]), // background color
         );
 
-        let rect = graphics::Rect::new(350.0, self.note_y, 100.0, 20.0);
-        let mesh = graphics::Mesh::new_rectangle(
+        let note_rect = graphics::Rect::new(380.0, self.note_y, 40.0, 20.0);
+        let note_mesh = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
-            rect,
+            note_rect,
+            Color::WHITE
+        )?;
+        canvas.draw(&note_mesh, Vec2::ZERO);
+
+        let mesh = graphics::Mesh::new_line(
+            ctx,
+            &[Vec2::new(0.0, self.hit_line_y), Vec2::new(win_w, self.hit_line_y)],
+            10.0,
             Color::WHITE,
         )?;
-        canvas.draw(&mesh, Vec2::new(0.0, 0.0));
-
+        canvas.draw(&mesh, Vec2::ZERO);
         // self.note_spawner.draw(&mut canvas, ctx)?;
         // self.score_manager.draw(ctx)?;
 
+        let score_text = graphics::Text::new(format!("Score: {}", self.score));
+        canvas.draw(&score_text, Vec2::new(10.0, 10.0));
+
         canvas.finish(ctx)?;
+        Ok(())
+    }
+
+    fn key_down_event(
+            &mut self,
+            _ctx: &mut Context,
+            input: ggez::input::keyboard::KeyInput,
+            _repeated: bool,
+        ) -> GameResult {
+        
+        if let Some(keycode) = input.keycode {
+            if keycode == ggez::input::keyboard::KeyCode::Space {
+                let diff = (self.note_y - self.hit_line_y).abs();
+                if diff < 10.0 {
+                    self.score += 100;
+                } else if diff < 30.0 {
+                    self.score += 50;
+                } else {
+                    self.score -= 10;
+                }
+                self.note_y = 0.0;
+            }
+        }
         Ok(())
     }
 }
@@ -65,7 +109,7 @@ pub fn main() -> GameResult {
         .window_setup(ggez::conf::WindowSetup::default().title("Rust Rhythm Game"))
         .window_mode(ggez::conf::WindowMode::default().dimensions(800.0, 600.0));
 
-    let (ctx, event_loop) = cb.build()?;
-    let state = GameState::new()?;
+    let (mut ctx, event_loop) = cb.build()?;
+    let state = GameState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
 }
